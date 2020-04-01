2ec2fac76504d20ca7afbf800a40b9f51ba5848a
javierferfer99@gmail.com <javierferfer99@gmail.com>
2020-04-01 12:54:39 +0200

    - Agregado clase para imagenes circulares - Agregado imagen por defecto para usuario
    - Add class to create circle images
    - Add default user image

--- TFG-Client/MainFormProgram.Designer.cs
+++ TFG-Client/MainFormProgram.Designer.cs
@@ -30,7 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFormProgram));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.myOwnCircleComponent1 = new TFG_Client.MyOwnCircleComponent();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myOwnCircleComponent1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -42,17 +44,29 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // myOwnCircleComponent1
+            // 
+            this.myOwnCircleComponent1.Image = ((System.Drawing.Image)(resources.GetObject("myOwnCircleComponent1.Image")));
+            this.myOwnCircleComponent1.Location = new System.Drawing.Point(430, 51);
+            this.myOwnCircleComponent1.Name = "myOwnCircleComponent1";
+            this.myOwnCircleComponent1.Size = new System.Drawing.Size(122, 117);
+            this.myOwnCircleComponent1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myOwnCircleComponent1.TabIndex = 1;
+            this.myOwnCircleComponent1.TabStop = false;
+            // 
             // MainFormProgram
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
             this.ClientSize = new System.Drawing.Size(615, 484);
+            this.Controls.Add(this.myOwnCircleComponent1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainFormProgram";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myOwnCircleComponent1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,6 +74,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private MyOwnCircleComponent myOwnCircleComponent1;
     }
 }
 
